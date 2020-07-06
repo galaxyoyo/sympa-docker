b685832d122e5c0d92b608a92cebefded5550ae8
Yohann D'anello <ynerant@crans.org>
2020-07-06 14:13:35 +0200

    :tada: Sympa is here!

---
+++ Dockerfile
@@ -0,0 +1,78 @@
+FROM debian:buster
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV RUNLEVEL=1
+
+RUN echo exit 0 > /usr/sbin/policy-rc.d && \
+	chmod +x /usr/sbin/policy-rc.d
+
+RUN apt update && \
+	apt install -y --no-install-recommends rsyslog && \
+	rm -rf /var/lib/apt/lists/*
+
+COPY rsyslog.conf /etc/rsyslog.conf
+RUN service rsyslog start
+
+RUN echo lists.example.com > /etc/mailname
+
+RUN apt update && \
+	apt install -yq --no-install-recommends perl \
+	nginx \
+	spawn-fcgi \
+	doc-base \
+	locales \
+	logrotate \
+	procps \
+	libdb5.1 \
+	procmail \
+	sasl2-bin \
+	postfix \
+	sympa && \
+	rm -rf /var/lib/apt/lists/*
+
+COPY rsyslog.d /etc/rsyslog.d
+
+COPY sympa.conf.template /etc/sympa/sympa/sympa.conf.template
+
+COPY main.cf /etc/postfix/main.cf
+COPY master.cf /etc/postfix/master.cf
+
+COPY entrypoint.sh /root/entrypoint.sh
+RUN chmod 0744 /root/entrypoint.sh
+ENTRYPOINT ["/root/entrypoint.sh"]
+
+RUN mkdir -p /etc/mail/sympa && \
+	mkdir -p /var/spool/sympa && \
+	mkdir -p /etc/sympa/robots && \
+	chown -R sympa:sympa /var/spool/sympa \
+	/etc/mail/sympa \
+	/var/spool/sympa \
+	/var/lib/sympa \
+	/etc/sympa/robots
+
+COPY list_aliases.tt2 /etc/sympa/list_aliases.tt2
+COPY transport.sympa.template /etc/sympa/transport.sympa.template
+COPY virtual.sympa.template /etc/sympa/virtual.sympa.template
+COPY robot.conf.template /etc/sympa/robot.conf.template
+COPY nginx.conf.template /etc/nginx/site.conf.template
+
+COPY wwsympa /etc/init.d/wwsympa
+RUN chmod +x /etc/init.d/wwsympa
+
+RUN touch /etc/sympa/transport.sympa \
+	/etc/sympa/virtual.sympa \
+	/etc/sympa/sympa_transport && \
+	chmod 0640 /etc/sympa/sympa_transport && \
+	chown sympa:sympa /etc/sympa/sympa_transport \
+		/etc/sympa/*.sympa
+
+RUN postmap hash:/etc/sympa/transport.sympa && \
+	postmap hash:/etc/sympa/virtual.sympa
+
+EXPOSE 25 80 465
+
+VOLUME /var/lib/sympa \
+	/var/spool/sympa \
+	/etc/sympa/robots
+
+ENV DOMAINS="localhost"
---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cat /etc/sympa/sympa/sympa.conf.template | sed "s/{{MAIN_LIST_DOMAIN}}/$MAIN_LIST_DOMAIN/g" | sed "s/{{LISTMASTERS}}/$LISTMASTERS/g" | sed "s/{{DB_TYPE}}/$DB_TYPE/g" | sed "s/{{DB_NAME}}/$DB_NAME/g" | sed "s/{{DB_HOST}}/$DB_HOST/g" | sed "s/{{DB_PORT}}/$DB_PORT/g" | sed "s/{{DB_USER}}/$DB_USER/g" | sed "s/{{DB_PASSWORD}}/$DB_PASSWORD/g" > /etc/sympa/sympa/sympa.conf
+
+chown -R sympa:sympa /etc/sympa/sympa_transport \
+	/var/spool/sympa \
+	/var/lib/sympa
+
+LIST_DOMAINS=$(echo $DOMAINS | tr ";" "\n")
+[[ ! -f /etc/sympa/transport.sympa ]] || rm /etc/sympa/transport.sympa
+[[ ! -f /etc/sympa/virtual.sympa ]] || rm /etc/sympa/virtual.sympa
+touch /etc/sympa/transport.sympa /etc/sympa/virtual.sympa
+rm /etc/nginx/sites-available/*
+rm /etc/nginx/sites-enabled/*
+
+for domain in $LIST_DOMAINS
+do
+	echo "Adding domain $domain..."
+	[[ -d /etc/sympa/$domain ]] || mkdir -m 0755 /etc/sympa/$domain
+	[[ -f /etc/sympa/robots/$domain.conf ]] || cat /etc/sympa/robot.conf.template | sed "s/{{MAILING_LIST_DOMAIN}}/$domain/g" > /etc/sympa/robots/$domain.conf
+	[[ -f /etc/sympa/$domain/robot.conf ]] || ln -s /etc/sympa/robots/$domain.conf /etc/sympa/$domain/robot.conf
+	chown -R sympa:sympa /etc/sympa/$domain /etc/sympa/robots
+	[[ -d /var/lib/sympa/list_data/$domain ]] || mkdir -m 0750 -p /var/lib/sympa/list_data/$domain
+	chown -R sympa:sympa /var/lib/sympa/list_data/$domain
+	cat /etc/sympa/transport.sympa.template | sed "s/{{MAILING_LIST_DOMAIN}}/$domain/g" >> /etc/sympa/transport.sympa
+	cat /etc/sympa/virtual.sympa.template | sed "s/{{MAILING_LIST_DOMAIN}}/$domain/g" >> /etc/sympa/virtual.sympa
+	cat /etc/nginx/site.conf.template | sed "s/{{MAILING_LIST_DOMAIN}}/$domain/g" > /etc/nginx/sites-available/$domain
+	ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/$domain
+	echo "Domain $domain added!"
+done
+
+chown -R www-data:www-data /etc/nginx/sites-available /etc/nginx/sites-enabled
+
+service rsyslog restart
+service postfix restart
+
+postmap hash:/etc/sympa/transport.sympa
+postmap hash:/etc/sympa/virtual.sympa
+service postfix reload
+
+/usr/lib/sympa/bin/sympa.pl --health_check
+/usr/lib/sympa/bin/sympa_newaliases.pl
+service sympa restart
+service wwsympa restart
+
+nginx -g "daemon off;"
